9d3134a58174705b3b5689675b3f3164d57d0fb8
Nathan Merrill <mathiscool3000@gmail.com>
2020-01-17 21:19:59 +0000

    Add basic cliparse parser

--- connectionmanager.py
+++ connectionmanager.py
@@ -1,7 +1,19 @@
 # Author: Nathan Merrill
-import yaml
+import cliparse, yaml
 
 CONFIG_FILE = '/home/nathan/.cmrc.yaml'
 
 with open(CONFIG_FILE, 'r') as stream:
     config = yaml.safe_load(stream)
+
+parser = cliparse.Parser()
+parser.setRequiredOperands(1)
+parser.setOperandConstraints({
+    0: [
+        'connect',
+        'scp',
+        'edit'
+    ]
+})
+args = parser.parseArgs()
+
